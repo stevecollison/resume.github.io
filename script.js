6cb3244e615e6ae0fb263bfd996d0ecd6b9b7e5a
stevecollison <collison.steven@gmail.com>
2017-10-15 15:20:13 -0400

    Update to .ca
    Several updates removing the coming soon logos and adding project
    imagery from BS

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 $(document).ready(function(){
-$('.span_1_of_3, .testimonial h1').mouseenter(function() {
-        $(this).fadeTo(0,1);
+$('.span_1_of_4, .testimonial h1').mouseenter(function() {
+        $(this).fadeTo(0,0.5);
     });
 
-$('.span_1_of_3, .testimonial h1').mouseleave(function() {
-    $(this).fadeTo('super fast', 0.5);
+$('.span_1_of_4, .testimonial h1').mouseleave(function() {
+    $(this).fadeTo('super fast', 1);
 });
 
-$('h1, h2, h3').stop(true).fadeIn({
-        duration: 5000,
-        queue: false
-    }).css('display', 'none').slideDown(1000);
+$('.span_1_of_5, .testimonial h1').mouseenter(function() {
+        $(this).fadeTo(0,0.5);
+    });
+
+$('.span_1_of_5, .testimonial h1').mouseleave(function() {
+    $(this).fadeTo('super fast', 1);
+});
+
+$('.span_1_of_6, .testimonial h1').mouseenter(function() {
+        $(this).fadeTo(0,0.5);
+    });
+
+$('.span_1_of_6, .testimonial h1').mouseleave(function() {
+    $(this).fadeTo('super fast', 1);
+});
+
+$('.span_1_of_7, .testimonial h1').mouseenter(function() {
+        $(this).fadeTo(0,0.5);
+    });
+
+$('.span_1_of_7, .testimonial h1').mouseleave(function() {
+    $(this).fadeTo('super fast', 1);
+});
+$('.span_1_of_8, .testimonial h1').mouseenter(function() {
+        $(this).fadeTo(0,0.5);
+    });
+
+$('.span_1_of_8, .testimonial h1').mouseleave(function() {
+    $(this).fadeTo('super fast', 1);
+});
+$('.span_1_of_9, .testimonial h1').mouseenter(function() {
+        $(this).fadeTo(0,0.5);
+    });
+
+$('.span_1_of_9, .testimonial h1').mouseleave(function() {
+    $(this).fadeTo('super fast', 1);
+});
+
+
 });
