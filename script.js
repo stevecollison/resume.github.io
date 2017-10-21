da268689a751f22e2a7858ed4dd419f3da4a6bfe
stevecollison <collison.steven@gmail.com>
2017-10-21 14:22:30 -0400

    Back to Steve

---
+++ script.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+
+$('.span_1_of_4, .testimonial h1').click(function() {
+        window.location = 'https://www.steven-collison.com/runup-mobile-app-design/';
+    });
+
+    $('.span_1_of_5, .testimonial h1').click(function() {
+            window.location = 'https://www.steven-collison.com/runup-desktop-marketing-website/';
+        });
+
+        $('.span_1_of_6, .testimonial h1').click(function() {
+                window.location = 'https://www.steven-collison.com/technometer/';
+            });
+
+            $('.span_1_of_7, .testimonial h1').click(function() {
+                    window.location = 'https://www.steven-collison.com/runup-mobile-marketing-site/';
+                });
+
+                $('.span_1_of_8, .testimonial h1').click(function() {
+                        window.location = 'https://www.steven-collison.com/taxi/';
+                    });
+
+                    $('.span_1_of_9, .testimonial h1').click(function() {
+                            window.location = 'https://www.steven-collison.com/the-weathernetwork-redesign/';
+                        });
+
+
+$('.span_1_of_4, .testimonial h1').mouseenter(function() {
+        $(this).fadeTo(0,0.5);
+    });
+
+$('.span_1_of_4, .testimonial h1').mouseleave(function() {
+    $(this).fadeTo('super fast', 1);
+});
+
+$('.span_1_of_5, .testimonial h1').mouseenter(function() {
+        $(this).fadeTo(0,0.5);
+    });
+
+$('.span_1_of_5, .testimonial h1').mouseleave(function() {
+    $(this).fadeTo('super fast', 1);
+});
+
+$('.span_1_of_6, .testimonial h1').mouseenter(function() {
+        $(this).fadeTo(0,0.5);
+    });
+
+$('.span_1_of_6, .testimonial h1').mouseleave(function() {
+    $(this).fadeTo('super fast', 1);
+});
+
+$('.span_1_of_7, .testimonial h1').mouseenter(function() {
+        $(this).fadeTo(0,0.5);
+    });
+
+$('.span_1_of_7, .testimonial h1').mouseleave(function() {
+    $(this).fadeTo('super fast', 1);
+});
+$('.span_1_of_8, .testimonial h1').mouseenter(function() {
+        $(this).fadeTo(0,0.5);
+    });
+
+$('.span_1_of_8, .testimonial h1').mouseleave(function() {
+    $(this).fadeTo('super fast', 1);
+});
+$('.span_1_of_9, .testimonial h1').mouseenter(function() {
+        $(this).fadeTo(0,0.5);
+    });
+
+$('.span_1_of_9, .testimonial h1').mouseleave(function() {
+    $(this).fadeTo('super fast', 1);
+});
+
+
+});
